482c2682068b158e97b82def76b09386cc9a4bdb
Azzouz-billel <billel.azzouz@nhsm.edu.dz>
2026-05-25 23:17:37 +0100

    Update vercel_wsgi.py

--- vercel_wsgi.py
+++ vercel_wsgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Replace 'detector' with your core folder name if it's different
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'detector.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scanner.settings')
 
 application = get_wsgi_application()
 app = application
